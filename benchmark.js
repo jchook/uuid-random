a370d825b3381eeb35b2fed111c2c86c972fd224
Wes Roberts <g0r6p@a.zinc.email>
2018-11-29 20:54:55 -0500

    Cleans-up benchmark

--- benchmark.js
+++ benchmark.js
@@ -9,17 +9,16 @@
 */
 var uuids = {
 
-	// Keeping up with competition, but gone now?
+	// No longer exists?
 	// 'an-uuid': require('an-uuid'),
 
-	// This is the "best" one
-	// deprecated
+	// Deprecated.
 	// 'node-uuid': require('node-uuid'),
 
-	// This doesn't even work?
+	// Doesn't even work anymore?
 	// 'performance-uuid': require('performance-uuid').uuid,
 
-	// Slightly slower
+	// Slightly slower.
 	'portable-uuid': require('portable-uuid'),
 
 	// Sooo slow, had to remove it..
@@ -37,7 +36,6 @@ var uuids = {
 
 };
 
-
 var i, start, seconds, ops = 1000000;
 
 for (lib in uuids) {
@@ -46,46 +44,3 @@ for (lib in uuids) {
 	seconds = ((+new Date) - start) / 1000;
 	console.log(lib, (ops/seconds) + " ops/sec");
 }
-
-
-// global
-// var i, start, seconds, ops = 10000000;
-//
-// // Test ours here
-// var uuid_random = require('./index');
-// start = + new Date;
-// for (i = 0; i < ops; i++) uuid_random();
-// seconds = ((+new Date) - start) / 1000;
-// console.log('uuid-random', (ops/seconds) + " ops/sec");
-//
-// start = + new Date;
-// for (i = 0; i < ops; i++) uuid_random.bin();
-// seconds = ((+new Date) - start) / 1000;
-// console.log('uuid-random-bin', (ops/seconds) + " ops/sec");
-//
-// var snowflake = require('./snowflake');
-// start = + new Date;
-// for (i = 0; i < ops; i++) snowflake();
-// seconds = ((+new Date) - start) / 1000;
-// console.log('snowflake', (ops/seconds) + " ops/sec");
-
-// simpleflakes
-// var simpleflakes = require('simpleflakes');
-// start = + new Date;
-// for (i = 0; i < ops; i++) simpleflakes.simpleflake();
-// seconds = ((+new Date) - start) / 1000;
-// console.log('simpleflakes', (ops/seconds) + " ops/sec");
-
-// simpleflake (slow as dick)
-// var simpleflake = require('simpleflake');
-// start = + new Date;
-// for (i = 0; i < ops; i++) simpleflake();
-// seconds = ((+new Date) - start) / 1000;
-// console.log('simpleflake', (ops/seconds) + " ops/sec");
-
-// node-uuid
-// var node_uuid = require('node-uuid');
-// start = + new Date;
-// for (i = 0; i < ops; i++) node_uuid();
-// seconds = ((+new Date) - start) / 1000;
-// console.log('node-uuid', (ops/seconds) + " ops/sec");
