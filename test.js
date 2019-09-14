b36892071b8f2a8b1829c519365783ffde420c36
Wes Roberts <g0r6p@a.zinc.email>
2019-09-14 18:08:47 -0400

    Expose clearBuffer() and randomBytes(). Refactor for performance.

--- test.js
+++ test.js
@@ -1,7 +1,15 @@
+var assert = require('assert');
 var uuid = require('./index');
+
+// Check format
 var i;
-for (i = 0; i<1000000; i++) {
-	if (!
-	  /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/.test(uuid())
-	) throw new Exception('Invalid UUID');
+for (i = 0; i<10000; i++) {
+	assert(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/.test(uuid()));
 }
+
+// Clear the buffer and change the randomBytes function to return 0s
+uuid.clearBuffer();
+uuid.randomBytes = function(length) {
+	return (new Array(length)).fill(0, 0, length);
+};
+assert(uuid() === '00000000-0000-4000-8000-000000000000');
